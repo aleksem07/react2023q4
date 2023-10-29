fde9b70b24a95cfd6cc6cfd6325c23877a47c32d
aleksem07 <aleksem07@gmail.com>
2023-10-30 02:29:58 +0300

    fix: resize loader gif

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,7 +2,7 @@ export const Loader = () => {
   return (
     <div className="loader mb-4 text-center">
       <p>Loading... Please wait</p>
-      <img src="vaderlukeREVISE.gif" />
+      <img src="vaderlukeREVISE.gif" alt="loader" className="w-100" />
     </div>
   );
 };
