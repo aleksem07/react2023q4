864837b8d986b1b567e7f8b551101b605deebbc4
aleksem07 <aleksem07@gmail.com>
2023-11-10 02:38:46 +0300

    refactor: del console.log in main.tsx

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -93,7 +93,7 @@ export default function Main() {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(data);
+
   return (
     <div className="main p-0 pb-0 container">
       <div className="main__container">
