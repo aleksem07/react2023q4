fbb851a77216f58c156b406576a7b05a6b1f2986
aleksem07 <aleksem07@gmail.com>
2023-11-19 21:59:41 +0300

    fix: del createAsyncThunk from heroes-slice

--- src/features/heroes/heroes-slice.ts
+++ src/features/heroes/heroes-slice.ts
@@ -1,32 +1,10 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import getHeroesAll from '../../services/heroes/heroes';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   heroes: [],
   status: false,
   currentHero: null,
 };
-
-type getHeroesAllProps = {
-  searchValue: string;
-  page: number;
-};
-
-export const getHeroes = createAsyncThunk(
-  'heroes/fetchHeroes',
-  async (
-    { searchValue, page }: getHeroesAllProps,
-    { rejectWithValue, dispatch }
-  ) => {
-    try {
-      const heroes = await getHeroesAll(searchValue, page);
-      dispatch(setHeroes(heroes.results));
-      return heroes;
-    } catch (error) {
-      return rejectWithValue(error);
-    }
-  }
-);
 
 export const heroesSlice = createSlice({
   name: 'heroes',
@@ -41,18 +19,6 @@ export const heroesSlice = createSlice({
     setCurrentHero: (state, action) => {
       state.currentHero = action.payload;
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(getHeroes.pending, (state) => {
-        console.log(state.heroes);
-        console.log('pending');
-      })
-      .addCase(getHeroes.fulfilled, (state, action) => {
-        state.heroes = action.payload;
-        console.log(state.heroes);
-        console.log('fulfilled');
-      });
   },
 });
 
