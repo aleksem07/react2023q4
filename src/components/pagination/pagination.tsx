b7b1e86965a495cab93e8addda3a3129b8b3011d
aleksem07 <aleksem07@gmail.com>
2023-11-24 02:14:32 +0300

    feat: add pagination component

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,27 +1,21 @@
 import styles from '@/styles/pagination.module.scss';
-import { PaginationProps } from './pagination.types';
 
 function Pagination() {
-
   return (
     <>
       <nav data-testid="pagination">
-        <ul className={styles.pagination + "justify-content-center gap-2"}>
-
-            <li className={styles.page_item + "disabled"}>
-              <button className="page-link text-dark">Prev</button>
-            </li>
-
+        <ul className={styles.pagination + 'justify-content-center gap-2'}>
+          <li className={styles.page_item + 'disabled'}>
+            <button className="page-link text-dark">Prev</button>
+          </li>
 
           <li className="page-item active-link ">
             <button className="page-link current">1</button>
           </li>
 
-
-            <li className="page-item disabled">
-              <button className="page-link text-dark">Next</button>
-            </li>
-
+          <li className="page-item disabled">
+            <button className="page-link text-dark">Next</button>
+          </li>
         </ul>
       </nav>
     </>
--- src/components/pagination/pagination.types.ts
+++ src/components/pagination/pagination.types.ts
@@ -1,9 +1,3 @@
-export type PaginationProps = {
-  fetchData: FetchData;
-  page: number;
-  onPageChange: (page: number) => void;
-};
-
 export type FetchData = {
   next: string | null;
   previous: string | null;
